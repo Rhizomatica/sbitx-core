d94049509329a4505a579e4967f7f4b0fdc203c6
Rafael Diniz <rafael@riseup.net>
2023-11-23 15:14:19 +0000

    adding more ideas

--- apps/radio.h
+++ apps/radio.h
@@ -24,13 +24,11 @@
 
 #include <stdint.h>
 #include <pthread.h>
+#include <stdbool.h>
 
-
-#define OPERATING_MODE_VOICE 0 // local operation
-#define OPERATING_MODE_DATA 1 // data operation
-
-#define RADIO_RX 0
-#define RADIO_TX 1
+#define OPERATING_MODE_FULL_VOICE 0 // DSP + IO, radio tx comes from MIC
+#define OPERATING_MODE_FULL_LOOPBACK 1 // DSP + IO, radio tx comes from Alsa loopback
+#define OPERATING_MODE_CONTROLS_ONLY 2 // just IO, does not open the Alsa device
 
 #define MODE_LSB 0
 #define MODE_USB 1
@@ -40,28 +38,40 @@
 #define AGC_MED 0
 #define AGC_FAST 0
 
+#define COMPRESSION_OFF 0
+#define COMPRESSION_ON 1
+
 typedef struct {
 
-    int radio_rx_tx; // tx or rx
+    uint32_t frequency;
+    uint16_t operating_mode; // FULL or CONTROLS_ONLY
 
-    int operating_mode; // ANALOG or DIGITAL
+    // In OPERATING_MODE_CONTROLS_ONLY, most of these will just be ignored...
+    uint16_t mode; // MODE_*
+    uint16_t agc; // AGC_*
+    uint16_t compression; // COMPRESSION_*
 
-    int analog_frequency;
-    int digital_frequency;
-    int frequency;
+    // Alsa levels
+    uint16_t mic_level; // 0 - 100
+    uint16_t rx_level;
+    uint16_t speaker_level; // 0 - 100
+    uint16_t tx_level;
 
-    int analog_mode;
-    int digital_mode;
-    int mode;
+    uint32_t bpf_low; // band-pass filter settings
+    uint32_t bpf_high;
 
+    uint32_t step_size;
 
-    int agc_analog; // OFF, SLOW, MED, FAST
-    int agc_digital;
-    int agc;
+    // These are switche that can enable/disable interaction with the devices
+    bool enable_knobs;
+    bool enable_ptt;
 
-    int mic_gain; // 0 - 100
+    // websocket streaming of radio parameters
+    bool enable_websocket;
 
-    int analog_to_digital_timeout; // in seconds
+    // do we need this?
+    bool is_default;
+    bool is_active;
 
     // oque fazer com o knob de frequencia no painel do radio?
     // bloquear o botao de freq quando tiver em digital? sim!
@@ -78,11 +88,10 @@ typedef struct {
     // radio config - operating mode (analog / digital)
     // oque fazer quando tiver ptt quando tiver no modo digital
 
-    int step_size;
-    int volume;
 
-    int serial_number;
-} radio_features;
+
+//    int serial_number;
+} radio_profile;
 
 
 
