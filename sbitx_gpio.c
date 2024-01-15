8758f4e684d8c143ddf52940b9f89c655cc1fd4d
Rafael Diniz <rafael@riseup.net>
2024-01-15 16:26:29 +0000

    more gpio init improvements

--- sbitx_gpio.c
+++ sbitx_gpio.c
@@ -49,7 +49,6 @@ void gpio_init(radio *radio_h)
         printf("Failed to initialise gpiolib - %d\n", ret);
         return ;
     }
-
     ret = gpiolib_mmap();
     if (ret)
     {
@@ -60,39 +59,21 @@ void gpio_init(radio *radio_h)
         return ;
     }
 
-    unsigned int pins[8] = {ENC1_A, ENC1_B, ENC1_SW, ENC2_A, ENC2_B, ENC2_SW, PTT, DASH};
+    // INPUT pins
+    unsigned int input_pins[8] = {ENC1_A, ENC1_B, ENC1_SW, ENC2_A, ENC2_B, ENC2_SW, PTT, DASH};
     for (int i = 0; i < 8; i++)
     {
-        gpio_set_fsel(pins[i], GPIO_FSEL_INPUT);
-        gpio_set_dir(pins[i], DIR_INPUT);
-        gpio_set_pull(pins[i], PULL_UP);
+        gpio_set_pull(input_pins[i], PULL_UP);
+        gpio_set_fsel(input_pins[i], GPIO_FSEL_INPUT);
     }
 
-    gpio_set_fsel(TX_LINE, GPIO_FSEL_OUTPUT);
-    gpio_set_dir(TX_LINE, DIR_OUTPUT);
-    gpio_set_fsel(LPF_A, GPIO_FSEL_OUTPUT);
-    gpio_set_dir(LPF_A, DIR_OUTPUT);
-    gpio_set_fsel(LPF_B, GPIO_FSEL_OUTPUT);
-    gpio_set_dir(LPF_B, DIR_OUTPUT);
-    gpio_set_fsel(LPF_C, GPIO_FSEL_OUTPUT);
-    gpio_set_dir(LPF_C, DIR_OUTPUT);
-    gpio_set_fsel(LPF_D, GPIO_FSEL_OUTPUT);
-    gpio_set_dir(LPF_D, DIR_OUTPUT);
-
-#ifdef SBITX_DE
-    gpio_set_fsel(TX_POWER, GPIO_FSEL_OUTPUT);
-    gpio_set_dir(TX_POWER, DIR_OUTPUT);
-#endif
-    //setup the LPFs and TX lines to initial state
-    gpio_set_drive(LPF_A, DRIVE_LOW);
-    gpio_set_drive(LPF_B, DRIVE_LOW);
-    gpio_set_drive(LPF_C, DRIVE_LOW);
-    gpio_set_drive(LPF_D, DRIVE_LOW);
-    gpio_set_drive(TX_LINE, DRIVE_LOW);
-
-#ifdef SBITX_DE
-    gpio_set_drive(TX_POWER, DRIVE_LOW); // not used in v2 and v3
-#endif
+    // OUTPUT pins
+    unsigned int output_pins[5] = {LPF_A, LPF_B, LPF_C, LPF_D, TX_LINE};
+    for (int i = 0; i < 5; i++)
+    {
+        gpio_set_drive(output_pins[i], DRIVE_LOW);
+        gpio_set_fsel(output_pins[i], GPIO_FSEL_OUTPUT);
+    }
 
     // Initialize our two encoder structs (front pannel knobs)
     enc_init(&radio_h->enc_a, ENC_FAST, ENC1_B, ENC1_A);
